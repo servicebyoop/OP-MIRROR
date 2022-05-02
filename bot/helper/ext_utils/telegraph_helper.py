a97f663aa25854e99ea39e61fb5f72be91822e71
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-05-03 00:47:40 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -73,7 +73,7 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Mirror-leech-bot Torrent Search',
+                title = 'OPGOHIL Torrent Search',
                 content=content
             )
         return
