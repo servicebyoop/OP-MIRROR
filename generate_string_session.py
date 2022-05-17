e4fea7cd28804daa14888f25adbfe45113559661
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-05-18 02:03:26 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -4,3 +4,4 @@ API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
 with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
     print(app.export_session_string())
+    #credit-opgohil 
