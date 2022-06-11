79fe7e0f94e7d9f78f19e98f22a156973693fc54
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-06-11 17:58:04 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -105,8 +105,8 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    p_str = '●' * cFull
-    p_str += ' ' * (12 - cFull)
+    p_str = '▰' * cFull
+    p_str += '▱' * (9 - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
